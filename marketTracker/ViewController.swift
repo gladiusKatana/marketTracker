057ebba3194f3ad984ac9faa229c9c4de9c46724
gladiusKatana <garthsnyder@protonmail.com>
2019-02-15 03:14:49 -0500

    Initial Commit

---
+++ marketTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marketTracker
+//
+//  Created by Garth Snyder on 2019-02-15.
+//  Copyright © 2019 Garth Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
